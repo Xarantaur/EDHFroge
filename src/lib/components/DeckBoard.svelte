225421ede468e880c0445cd9495823bde46eaa30
Lars <lahansen@live.dk>
2025-06-06 20:32:51 +0200

    quick fix for scaling of deckboard on mobile

--- src/lib/components/DeckBoard.svelte
+++ src/lib/components/DeckBoard.svelte
@@ -7,7 +7,7 @@
 </script>
 
 <div class="w-full px-4">
-	<div class="bg-gray-200 shadow p-6 max-h-[80vh] overflow-y-auto rounded">
+	<div class="bg-gray-200 shadow p-6 sm:max-h-[80vh] sm:overflow-y-auto rounded">
         <TileHeader title={title} subtitle={`CardCount: ${deckSize}`}></TileHeader>
 		<div class="h-full overflow-hidden 
                     [column-width:160px] 
